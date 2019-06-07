da0d0c0a0133ddb276b600d176edca52a7af49d2
ditarud <ditarud@miuandes.cl>
2019-06-07 19:05:55 -0400

    Navigation drawer and login

--- app/src/main/java/com/example/apptienda/MainActivity.kt
+++ app/src/main/java/com/example/apptienda/MainActivity.kt
@@ -1,12 +1,70 @@
 package com.example.apptienda
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setToolbar()
+        goToLoginActivity()
+    }
+
+    private fun goToLoginActivity(){
+        startActivityForResult(
+            Intent(this, LoginActivity::class.java),
+            RequestCode.GO_TO_LOGIN_FROM_MAIN_ACTIVITY.value)
+    }
+
+    private fun setToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+        }
+        setupNavViewListener()
+    }
+
+    private fun setupNavViewListener() {
+        navView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            drawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+            val transaction = supportFragmentManager.beginTransaction()
+            when (menuItem.itemId) {
+
+                R.id.signOut -> {
+                    onSignOut()
+                }
+            }
+
+            transaction.commit()
+            true
+        }
+    }
+
+    // Called when navDrawer is opened (used to display the menu)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+   fun onSignOut() {
+        goToLoginActivity()
     }
 }
---
+++ app/src/main/java/com/example/apptienda/RequestCode.kt
@@ -0,0 +1,5 @@
+package com.example.apptienda
+
+enum class RequestCode(val value: Int) {
+    GO_TO_LOGIN_FROM_MAIN_ACTIVITY(1)
+}
