adc0458d30d77cd87c9e779d5d1c0556511f704b
ditarud <ditarud@miuandes.cl>
2019-06-11 13:09:25 -0400

    Shopping History

--- app/src/main/java/com/example/apptienda/ShoppingHistoryFragment.kt
+++ app/src/main/java/com/example/apptienda/ShoppingHistoryFragment.kt
@@ -7,6 +7,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_shopping_history.*
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import java.net.URL
 
 
 class ShoppingHistoryFragment : Fragment() {
@@ -16,12 +20,20 @@ class ShoppingHistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_shopping_history, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setCredentials()
+        productNameTextView.text = "Computador 1"
+        orderDateTextView.text = "2019-10-12"
+        amountPaidTextView.text = "$23.000"
+        productPhotoImageView.setImageResource(R.mipmap.ic_launcher)
+
+
+
     }
 
     override fun onStart() {
