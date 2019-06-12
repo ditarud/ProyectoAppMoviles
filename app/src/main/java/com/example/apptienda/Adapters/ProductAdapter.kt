e239174bf879c90a046b7a6ecf41f698f1856072
juanesgh <31229915+juanesgh@users.noreply.github.com>
2019-06-11 22:56:22 -0400

    Product Frgament

---
+++ app/src/main/java/com/example/apptienda/Adapters/ProductAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.apptienda.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.example.apptienda.R
+import com.example.apptienda.db.models.Product
+
+class ProductAdapter(
+    context: Context,
+    private val dataSource: ArrayList<Product>) : BaseAdapter() {
+
+    private val inflater: LayoutInflater
+            = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return dataSource[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        // Get view for row item
+        val rowView = inflater.inflate(R.layout.list_item_product, parent, false)
+        rowView.findViewById<TextView>(R.id.nameTextView).text = dataSource[position].name
+        rowView.findViewById<TextView>(R.id.priceTextView).text = (dataSource[position].price).toString()
+        rowView.findViewById<TextView>(R.id.descriptionTextView).text = dataSource[position].description
+        return rowView
+    }
+}
--- app/src/main/java/com/example/apptienda/Fragments/ProductsFragment.kt
+++ app/src/main/java/com/example/apptienda/Fragments/ProductsFragment.kt
@@ -4,15 +4,23 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.example.apptienda.R
 import com.example.apptienda.db.AppDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+
+
+import com.example.apptienda.Adapters.ProductAdapter
+import com.example.apptienda.db.models.Product
+import kotlinx.android.synthetic.main.fragment_products.*
+import java.lang.Exception
 
 
 class ProductsFragment : Fragment() {
@@ -27,6 +35,53 @@ class ProductsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        loadProducts()
+        //setListOnClickListeners()
+    }
+
+    private fun loadProducts(){
+        val totalProducts = AppDatabase.getDatabase(context!!).productDao()
+
+        GlobalScope.launch(Dispatchers.IO) {  // replaces doAsync (runs on another thread)
+            val cant = totalProducts.getAll()
+            if (cant.size == 0){
+                try {
+                    val firstp = createProductObject()
+                    totalProducts.insert(firstp)
+                    launch(Dispatchers.Main) {
+                        Toast.makeText(context!!, "Saved successfully the product", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: Exception) {
+                    launch(Dispatchers.Main) {
+                        Log.d("ERROR", "Error storing product ${e.message}")
+                        Toast.makeText(context!!, "Error storing example products ${e.message}" , Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+
+        //val productsDao = AppDatabase.getDatabase(context!!).productDao()
+        GlobalScope.launch(Dispatchers.IO) {
+            val products = totalProducts.getAll()
+            launch(Dispatchers.Main){
+                val itemsAdapter = ProductAdapter(context!!, ArrayList(products))
+                productsListView.adapter = itemsAdapter
+            }
+        }
+    }
+
+    private fun setListOnClickListeners(){
+
+    }
+
+    private fun createProductObject(): Product{
+        val name = "Example"
+        val price = 1000
+        val description = "the very first product"
+        val stock = 1000
+        val deleted = false
+        return Product(name, price, description, stock, deleted)
     }
 
 
